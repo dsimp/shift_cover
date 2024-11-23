f81a3a3df5c5bd4744d63548a088d91d7f9e3e52
Djvon Simpson <55302995+dsimp@users.noreply.github.com>
2024-11-23 00:11:59 +0000

    edit gemfile

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,3 +4,12 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
+#!/bin/bash
+set -e
+
+# Clear previous gems to avoid version conflicts
+rm -rf .bundle
+rm -rf vendor/bundle
+
+# Install gems
+bundle install --path vendor/bundle
